0ba18f3dcdc5e489902f2efce65eeec891ccca04
digmen <digmen68@gmail.com>
2024-06-29 04:13:06 +0000

    sdfsdfsdfsdd

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
     const [loginState, setLoginState] = useState(false);
 
     return (
-        <div className='flex flex-col justify-center items-center mt-[80px] mx-auto max-w-[320px]'>
+        <div className='flex flex-col justify-center items-center mt-[20px] mx-auto max-w-[320px]'>
             {loginState ? <SignIn /> : <SignUp />}
             <button className='bg-white  w-full active:transition transition rounded-2xl ease-in-out duration-500 active:rounded-2xl p-3 border active:text-white active:bg-black' onClick={() => setLoginState(prev => !prev)}> {loginState ? 'Зарегестрироваться' : 'Войти'}</button>
         </div>
